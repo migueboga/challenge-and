8cf9d199e353538d7a247c3c461e80a82cc0b4d9
Miguel Bogarin <miguexz_163@hotmail.com>
2024-08-06 13:36:16 -0600

    Create module: storage. Add dependencies for module: storage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "Movies"
 include(":app")
 include(":moviedb")
+include(":storage")
---
+++ storage/src/main/java/com/op/storage/di/StorageDependencyProvider.kt
@@ -0,0 +1,29 @@
+package com.op.storage.di
+
+import android.content.Context
+import androidx.room.Room
+import com.op.storage.data.database.MovieDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object StorageDependencyProvider {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): MovieDatabase = Room.databaseBuilder(
+        context,
+        MovieDatabase::class.java,
+        MovieDatabase.DATABASE_NAME
+    )
+        .fallbackToDestructiveMigration()
+        .build()
+
+}
