0669ee5af46cf70c9c409e357e0f80f5cc2ff347
Miguel Bogarin <miguexz_163@hotmail.com>
2024-08-08 06:09:08 -0600

    Fix: Flow calls in cascade has issues

--- app/src/main/java/com/op/movies/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/op/movies/presentation/home/HomeViewModel.kt
@@ -2,16 +2,31 @@ package com.op.movies.presentation.home
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.op.moviedb.domain.entity.Movie
 import com.op.moviedb.domain.usecase.GetPopularMovieUseCase
 import com.op.moviedb.domain.usecase.GetRecommendationsByMovieIdUseCase
 import com.op.moviedb.domain.usecase.GetTopRatedMovieUseCase
 import com.op.movies.R
+import com.op.movies.data.strategy.DataAccessStrategy
+import com.op.movies.domain.GetPopularMovieStrategyUseCase
 import com.op.movies.presentation.base.BaseViewModel
+import com.op.storage.data.database.entity.PopularMovie
+import com.op.storage.data.database.entity.RecommendMovie
+import com.op.storage.data.database.entity.TopRatedMovie
+import com.op.storage.domain.usecase.GetPopularMovieLocalUseCase
+import com.op.storage.domain.usecase.GetRecommendMovieLocalUseCase
+import com.op.storage.domain.usecase.GetTopRatedMovieLocalUseCase
+import com.op.storage.domain.usecase.SetPopularMovieLocalUseCase
+import com.op.storage.domain.usecase.SetRecommendMovieLocalUseCase
+import com.op.storage.domain.usecase.SetTopRatedMovieLocalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,48 +34,32 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val getPopularMovieUseCase: GetPopularMovieUseCase,
     private val getTopRatedMovieUseCase: GetTopRatedMovieUseCase,
-    private val getRecommendationsByMovieIdUseCase: GetRecommendationsByMovieIdUseCase
+    private val getRecommendationsByMovieIdUseCase: GetRecommendationsByMovieIdUseCase,
+    private val setPopularMovieLocalUseCase: SetPopularMovieLocalUseCase,
+    private val setRecommendMovieLocalUseCase: SetRecommendMovieLocalUseCase,
+    private val getPopularMovieLocalUseCase: GetPopularMovieLocalUseCase,
+    private val getRecommendMovieLocalUseCase: GetRecommendMovieLocalUseCase,
+    private val getTopRatedLocalUseCase: GetTopRatedMovieLocalUseCase,
+    private val setTopRatedMovieLocalUseCase: SetTopRatedMovieLocalUseCase
 ) : BaseViewModel<HomeUiState>(HomeUiState()) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    fun getPopularMovies() {
-        viewModelScope.launch {
-            _uiState.postValue(_uiState.value?.copy(isLoading = true))
-            getPopularMovieUseCase()
-                .flatMapConcat { popularMovies ->
-                    val movieId = popularMovies.first().id.toString()
-                    getRecommendationsByMovieIdUseCase(movieId).map {
-                        popularMovies to it
-                    }
-                }
-                .catch {
-                    Log.e("debug", it.localizedMessage, it)
-                    _uiState.postValue(
-                        _uiState.value?.copy(
-                            isLoading = false, error = R.string.err_get_popular_movies
-                        )
-                    )
-                }
-                .collect { (popularMovies, recommendations) ->
-                    _uiState.postValue(
-                        _uiState.value?.copy(
-                            isLoading = false,
-                            popularMovieList = popularMovies.map {
-                                MovieUiState(it.poster_path, it.title)
-                            },
-                            recommendationList = recommendations.map {
-                                MovieUiState(it.poster_path, it.title)
-                            }
-                        )
-                    )
-                }
-        }
-    }
-
     fun getTopRatedMovies() {
         viewModelScope.launch {
             _uiState.postValue(_uiState.value?.copy(isLoading = true))
-            getTopRatedMovieUseCase()
+            getTopRatedLocalUseCase()
+                .distinctUntilChanged()
+                .flatMapConcat { localTopRated ->
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            isLoading = false,
+                            topRatedMovieList = localTopRated.map {
+                                MovieUiState(it.path, it.title)
+                            }
+                        )
+                    )
+                    getTopRatedMovieUseCase()
+                }
                 .catch {
                     Log.e("debug", it.localizedMessage, it)
                     _uiState.postValue(
@@ -70,6 +69,7 @@ class HomeViewModel @Inject constructor(
                     )
                 }
                 .collect { response ->
+                    saveLocalTopRatedMovies(response)
                     _uiState.postValue(
                         _uiState.value?.copy(
                             isLoading = false,
@@ -79,6 +79,97 @@ class HomeViewModel @Inject constructor(
                         )
                     )
                 }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun getPopularMovies() {
+        viewModelScope.launch {
+            _uiState.postValue(_uiState.value?.copy(isLoading = true))
+            getPopularMovieLocalUseCase()
+                .distinctUntilChanged()
+                .flatMapConcat { local ->
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            popularMovieList = local.map {
+                                MovieUiState(it.path, it.title)
+                            }
+                        )
+                    )
+                    getPopularMovieUseCase()
+                }
+                .flatMapConcat { popularMovies ->
+                    saveLocalPopularMovies(popularMovies)
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            popularMovieList = popularMovies.map {
+                                MovieUiState(it.poster_path, it.title)
+                            }
+                        )
+                    )
+                    getRecommendMovieLocalUseCase().map {
+                        it to popularMovies.first().id.toString()
+                    }
+                }
+                .distinctUntilChanged()
+                .flatMapConcat { (recommendedLocal, movieId) ->
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            recommendationList = recommendedLocal.map {
+                                MovieUiState(it.path, it.title)
+                            }
+                        )
+                    )
+                    getRecommendationsByMovieIdUseCase(movieId)
+                }
+                .catch {
+                    Log.e("debug", it.localizedMessage, it)
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            isLoading = false, error = R.string.err_get_popular_movies
+                        )
+                    )
+                }
+                .collect { remoteRecommendations ->
+                    saveLocalRecommendationMovies(remoteRecommendations)
+                    _uiState.postValue(
+                        _uiState.value?.copy(
+                            recommendationList = remoteRecommendations.map {
+                                MovieUiState(it.poster_path, it.title)
+                            }
+                        )
+                    )
+                }
+        }
+    }
+
+    override fun clearError() {
+        _uiState.postValue(
+            _uiState.value?.copy(error = null)
+        )
+    }
+
+    private fun saveLocalPopularMovies(popularMovieList: List<Movie>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            setPopularMovieLocalUseCase(popularMovieList.map {
+                PopularMovie(it.id.toLong(), it.poster_path, it.title)
+            })
+        }
+    }
+
+    private fun saveLocalRecommendationMovies(recommendationMovieList: List<Movie>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            setRecommendMovieLocalUseCase(recommendationMovieList.map {
+                RecommendMovie(it.id.toLong(), it.poster_path, it.title)
+            })
+        }
+    }
+
+    private fun saveLocalTopRatedMovies(topRatedList: List<Movie>) {
+        viewModelScope.launch(Dispatchers.IO)  {
+            setTopRatedMovieLocalUseCase(
+                topRatedList.map { TopRatedMovie(it.id.toLong(), it.poster_path, it.title) }
+            )
         }
     }
 
